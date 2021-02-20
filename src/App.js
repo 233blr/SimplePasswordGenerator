aff4c103498896f55ce9a9cfced760f1e66bbf89
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-02-20 04:51:07 +0300

    Added project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [pass, setPass] = useState('Password Generator')
+  const [passLength, setPassLength] = useState(15);
+  const [passCopied, setPassCopied] = useState(false);
+
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ';
+  const symbols = '!@#$%^&()_+?:{}[]';
+
+  const rangeValue = (e) => setPassLength(e.target.value);
+
+  const handlePassCopy = () => {
+    if (pass !== 'Password Generator') {
+      let timerId = null;
+      navigator.clipboard.writeText(pass)
+      setPassCopied(true)
+      setTimeout(() => setPassCopied(false),
+        1000)
+    }
+  };
+
+  const handelPassGenerator = () => {
+    let result = '';
+    if (passLength > 10) chars += symbols;
+    for (let i = 0; i < passLength; i++) {
+      const runNum = Math.floor(Math.random() * chars.length);
+      result += chars.substring(runNum, runNum + 1)
+    }
+    setPass(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {passCopied && <span>Copied !</span>}
+      <h1 onClick={handlePassCopy} className='pass_output'>{pass}</h1>
+      <div>
+        <p>password complexity</p>
+        <input type="range" min="5" max="20" step="5" onChange={rangeValue} />
+      </div>
+      <button onClick={handelPassGenerator}>Generate</button>
     </div>
   );
 }
