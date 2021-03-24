480adbac5e7b59a0190afbec5678377a1114bb7c
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-03-25 02:47:44 +0300

    Fixed some code

--- src/App.js
+++ src/App.js
@@ -9,19 +9,8 @@ const App = () => {
   let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ';
   const symbols = '!@#$%^&()_+?:{}[]';
 
-  const rangeValue = (e) => setPassLength(e.target.value);
-
-  const handlePassCopy = () => {
-    if (pass !== 'Password Generator') {
-      let timerId = null;
-      navigator.clipboard.writeText(pass)
-      setPassCopied(true)
-      setTimeout(() => setPassCopied(false),
-        1000)
-    }
-  };
-
-  const handelPassGenerator = () => {
+  const rangeValue = (event) => {
+    setPassLength(event.target.value);
     let result = '';
     if (passLength > 10) chars += symbols;
     for (let i = 0; i < passLength; i++) {
@@ -31,15 +20,24 @@ const App = () => {
     setPass(result);
   }
 
+  const handlePassCopy = () => {
+    if (pass !== 'Password Generator') {
+      navigator.clipboard.writeText(pass)
+      setPassCopied(true)
+      setTimeout(() => setPassCopied(false),
+        1000)
+    }
+  };
+
   return (
     <div className="App">
       {passCopied && <span>Copied !</span>}
       <h1 onClick={handlePassCopy} className='pass_output'>{pass}</h1>
       <div>
         <p>password complexity</p>
-        <input type="range" min="5" max="20" step="5" onChange={rangeValue} />
+        <p className='message'>move to generate</p>
+        <input type="range" min="5" max="25" step="1" onChange={rangeValue} />
       </div>
-      <button onClick={handelPassGenerator}>Generate</button>
     </div>
   );
 }
